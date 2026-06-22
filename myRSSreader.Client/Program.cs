002fbc42fc1f7c9d19d0a23f6ec8040832b033ee
Ja <ejawah@wp.pl>
2026-06-22 18:58:44 +0200

    change for easier development

--- myRSSreader.Client/Program.cs
+++ myRSSreader.Client/Program.cs
@@ -5,9 +5,11 @@ using myRSSreader.Client;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+ #if DEBUG
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5152/") });
+ #else
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://my-rss-reader-api.onrender.com/") });
-
+#endif
 //builder.Services.AddStorageExtensions();
 
 await builder.Build().RunAsync();
